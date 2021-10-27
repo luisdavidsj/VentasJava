ddc1b083320bb312d21c32dc68cde992141794cd
Luis David Saenz <luisdavidspider@gmail.com>
2021-10-27 09:54:02 -0500

    Sección 22: Manejo de clases en Java

---
+++ src/mx/com/gm/ventas/test/VentasTest.java
@@ -0,0 +1,23 @@
+package mx.com.gm.ventas.test;
+
+import mx.com.gm.ventas.*;
+
+public class VentasTest {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Camisa",50.00);
+        Producto producto2 = new Producto("Pantalon",100.00);
+        
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+        
+        //**
+        Producto producto3 = new Producto("Guantes",200.00);
+        Orden orden2 = new Orden();
+        orden2.agregarProducto(producto1);
+        orden2.agregarProducto(producto2);
+        orden2.agregarProducto(producto3);
+        orden2.mostrarOrden();
+    }
+}
